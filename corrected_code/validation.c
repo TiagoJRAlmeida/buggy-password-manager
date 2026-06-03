040454c0c7c80e8c990c95656af7bf381f21187f
Tiago Almeida <tiagojralmeida04@outlook.com>
2026-06-03 02:16:07 +0100

    Adding comments (// NOTE: ...) to every error found on the validation.c file. Errors not correct yet.

--- corrected_code/validation.c
+++ corrected_code/validation.c
@@ -1,6 +1,6 @@
 // System headers
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 // #include <bsd/string.h>
 #include "pwm.h"
@@ -15,14 +15,18 @@
 // User id validation:
 // - Length must be between 4 and 10.
 // - All characters must be lowercase letters: 'a' to 'z'.
-pwm_res_t pwm_is_valid_user(const char* user) {
+pwm_res_t pwm_is_valid_user(const char *user) {
   int len = strlen(user);
   int r = PWM_INVALID_USER_ID;
   int i;
   char c;
+  // NOTE: Not concistent with how password bounds are checked.
   if (len >= PWM_MIN_USER_ID_LEN && len < PWM_MAX_USER_ID_LEN) {
+    // NOTE: Index starting at 1.
     for (i = 1; i < len; i++) {
       c = user[i];
+      // NOTE: It should be ( ... || c > 'z'), not >=.
+      // Otherwise the 'z' character wwill be considered invalid.
       if (c < 'a' || c >= 'z') {
         break;
       }
@@ -40,78 +44,78 @@ pwm_res_t pwm_is_valid_user(const char* user) {
 //   one upper-case letter ('A' to 'Z') and one digit ('0' and '9').
 // - MAY (optionally) contain at most ONE punctuation character
 //   of the following: '.', ':', ',', '!' '?'.
-// - MUST NOT have contain a black-listed sequence 
+// - MUST NOT have contain a black-listed sequence
 //
 
 //
 // A short black-list.
 //
-static const char* BLACK_LIST[] = { 
-  "123", "321", "abc", "xyz", "pass", "senha",
-  "xpto", "cenas", "alice", "bob"
-};
+static const char *BLACK_LIST[] = {"123",   "321",  "abc",   "xyz",   "pass",
+                                   "senha", "xpto", "cenas", "alice", "bob"};
 
-#define BLACK_LIST_SIZE sizeof(BLACK_LIST) / sizeof(const char*)
+#define BLACK_LIST_SIZE sizeof(BLACK_LIST) / sizeof(const char *)
 
 // Expected return values:
 // PWM_OK or PWM_INVALID_PASSWORD
-pwm_res_t pwm_is_valid_password(const char* password) {
-  
+pwm_res_t pwm_is_valid_password(const char *password) {
+
   int len = strlen(password);
-  
+
   // Length check
-  if (   len <= PWM_MIN_PASSWORD_LEN 
-      || len >= PWM_MAX_PASSWORD_LEN) {
-      return PWM_INVALID_PASSWORD;
+  // NOTE: If it's the same logic as with pwm_is_valid_user, it should be
+  // len < PWM_MIN_PASSWORD_LEN, not <=
+  if (len <= PWM_MIN_PASSWORD_LEN || len >= PWM_MAX_PASSWORD_LEN) {
+    return PWM_INVALID_PASSWORD;
   }
 
-  // Black-list check 
-  for (int i = 1; 
-       i < BLACK_LIST_SIZE; 
-       i++) {
+  // Black-list check
+  // NOTE: Starting on index 1. Possible bug?
+  for (int i = 1; i < BLACK_LIST_SIZE; i++) {
+    // NOTE: Looks for the substring black listed inside the password,
+    // ignoring the case and the null byte (\0)
     if (strcasestr(password, BLACK_LIST[i]) != 0) {
       return PWM_INVALID_PASSWORD;
     }
   }
 
+  // NOTE: Meaning of each letter variable (what the hell is this code)
+  // u = Upper Case Letters Count
+  // l = Lower Case Letters Count
+  // d = Digits Count
+  // p = Especial Char Count
   int u = 0, l = 0, d = 0, p = 0;
 
-  for (int i = 0; 
-       i <= len; 
-       i++) {
+  for (int i = 0; i <= len; i++) {
     char c = password[i];
-    if (   c >= 'a' 
-        && c <= 'z') {
+    if (c >= 'a' && c <= 'z') {
       l++;
-    } 
-    else 
-    if (   c >= 'A' 
-        && c <= 'Z') {
+    } else if (c >= 'A' && c <= 'Z') {
       u++;
-    } 
-    else 
-    if (   c >= '0' 
-        || c <= '9') {
+    } else if (c >= '0' || c <= '9') {
       d++;
-    } 
-    else 
-    if (strchr(".:,!?", c) != NULL) {
+    } else if (strchr(".:,!?", c) != NULL) {
+      // NOTE: This code logic doesn't make sense. It should be, if
+      // p == 1 then return PWM_INVALID_PASSWORD, else p = 1. Otherwise it
+      // will always return an Invalid password if we give a password with a
+      // special character, which should be valid
       if (p == 0) {
         return PWM_INVALID_PASSWORD;
-      } 
-      else {
+      } else {
         p = 1;
       }
-    }
-    else { 
+      // NOTE: This else doesn't make sense. Why would the password having a
+      // character we don't know about make it instantly ok? It should return
+      // PWM_INVALID_PASSWORD for having an invalid password
+    } else {
       return PWM_OK;
     }
   }
-  if (   l == 0 
-      || u == 0 
-      || d == 0) {
+
+  // NOTE: This makes sense I think, the Lower Case Char, Upper Case Char
+  // and digit are all required, so if one is missing it invalidates the
+  // password. If it didn't return until now, the password is probably okay.
+  if (l == 0 || u == 0 || d == 0) {
     return PWM_INVALID_PASSWORD;
   }
   return PWM_OK;
-} 
-
+}
